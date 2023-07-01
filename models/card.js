176fe86102783ddbd9d14d6d94cb270bd1d94916
Leisan Andriyanova <andriyanova.lm@yandex.ru>
2023-07-01 12:24:24 +0300

    add validator

--- models/card.js
+++ models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const cardShema = new mongoose.Schema({
   name: {
@@ -9,6 +10,10 @@ const cardShema = new mongoose.Schema({
   },
   link: {
     type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
     required: [true, 'Поле "link" должно быть заполнено'],
   },
   owner: {
