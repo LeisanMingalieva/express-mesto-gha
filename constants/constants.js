3f5b8694d4e5eb9b61e6be84853b2e612efb6e41
Leisan Andriyanova <andriyanova.lm@yandex.ru>
2023-07-12 13:21:58 +0300

    add REG_EXP

--- constants/constants.js
+++ constants/constants.js
@@ -3,7 +3,7 @@ const NOT_FOUND_ERROR_CODE = 404;
 const BAD_REQUEST_ERROR_CODE = 400;
 const CREATED_CODE = 201;
 
-const REG_EXP = '/https?:\/\/(www\.)?[]/';
+const REG_EXP = '/https?:\/\/(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/';
 
 module.exports = {
   INTERNAL_SERVER_ERROR_CODE,
