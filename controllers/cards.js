a7bca88fd03c38cdad942e6ae24f47e03c94dc96
Leisan Andriyanova <andriyanova.lm@yandex.ru>
2023-07-14 12:48:52 +0300

    fix delete card

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,6 +5,9 @@ const {
   BAD_REQUEST_ERROR_CODE,
   CREATED_CODE,
 } = require('../constants/constants');
+const NotFoundError = require('../helpers/errors/NotFoundError');
+const ForbiddenError = require('../helpers/errors/ForiddenError');
+const BadRequestError = require('../helpers/errors/BadRequestError');
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
@@ -33,18 +36,38 @@ const getCards = (req, res) => {
     });
 };
 
-const deleteCard = (req, res) => {
-  const { cardId } = req.params;
-  Card.findByIdAndRemove(cardId)
-    .orFail(new Error('NotValidId'))
+// const deleteCard = (req, res) => {
+//   const { cardId } = req.params;
+//   Card.findByIdAndRemove(cardId)
+//     .orFail(new Error('NotValidId'))
+//     .then((card) => {
+//       res.send(card);
+//     })
+//     .catch((err) => {
+//       if (err.message === 'NotValidId') {
+//         res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Такой карточки не существует' });
+//       } else {
+//         res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Ошибка сервера' });
+//       }
+//     });
+// };
+const deleteCard = (req, res, next) => {
+  Card.findById(req.params.cardId)
     .then((card) => {
-      res.send(card);
+      if (!card) {
+        throw new NotFoundError('Такой карточки не существует');
+      }
+      if (req.user.id._id === card.owner.toString()) {
+        return card.deleteOne();
+      }
+      throw new ForbiddenError('Вы не можете удалить карточку');
     })
+    .then((removedCard) => res.send(removedCard))
     .catch((err) => {
-      if (err.message === 'NotValidId') {
-        res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Такой карточки не существует' });
+      if (err.name === 'Cast Error') {
+        next(new BadRequestError('переданы невалидные данные'));
       } else {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Ошибка сервера' });
+        next(err);
       }
     });
 };
